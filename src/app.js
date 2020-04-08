22ed46ef10d60e820cea918976b31157ecd6c1fc
Marcelo Henrique <marcelohs_93@hotmail.com>
2020-04-08 22:52:35 +1000

    Add methods GET, POST and PUT

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,15 +10,49 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url, 
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.send(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id)
+
+  if (repositoryIndex < 0){
+    return response.status(400).json({error: 'ID Could not be found'})
+  } 
+
+  const repository = {
+    id, 
+    title, 
+    url, 
+    techs,
+     likes: repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex] = repository
+  
+  return response.json(repository)
+
 });
 
 app.delete("/repositories/:id", (req, res) => {
